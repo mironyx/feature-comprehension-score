bfb168a8bce16ad442419315dcd62adea584c1e9
Leonid Sokolovsky <lg.sokolovsky@gmail.com>
2026-03-23 22:10:45 +0000

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,12 +9,12 @@ export async function middleware(
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
+  let i=;
   if (!user) {
     const signInUrl = new URL('/auth/sign-in', request.url);
     return NextResponse.redirect(signInUrl);
   }
-
+  
   return response;
 }
 
